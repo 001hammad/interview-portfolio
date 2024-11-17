b7c5fd6596825bd640a32ae238bec1c190d54cb5
001hammad <167647584+001hammad@users.noreply.github.com>
2024-11-18 00:16:35 +0500

    Update Hero.tsx

--- app/MainComponents/Hero.tsx
+++ app/MainComponents/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         {/* Text and Description */}
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
-            I&apos;m Hammad, a passionate frontend developer creating seamless user experiences.
+          Hi, I&apos;m Hammad — a passionate frontend developer dedicated to crafting seamless and engaging user experiences.
             <br className="hidden lg:inline-block" />
             with Us
           </h1>
